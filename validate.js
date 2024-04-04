15cb9bb5d9e83916c802076a24e49b59dbdc02fa
Phutanes <phutanestrisiri@gmail.com>
2024-04-04 13:09:10 +0700

    1st commit fixed

--- validate.js
+++ validate.js
@@ -1,6 +1,6 @@
 function checkHeight() {
   let h = (document.getElementById("myHeight").value).trim();
-  if (!isNaN(h)) {
+  if (!isNaN(h) && h >= 1 && h <= 200) {
     return true;
   } else {
 	return false;
@@ -9,7 +9,7 @@ function checkHeight() {
 
 function checkInseam() {
   let i = (document.getElementById("myInseam").value).trim();
-  if (!isNaN(i)) {
+  if (!isNaN(i) && i >= 1 && i <= 100) {
     return true;
   } else {
 	return false;
