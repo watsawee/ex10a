e7f05d80e32a5d32563bf33227441482fdc5053c
koonkit <kanghs112211@gmail.com>
2024-04-04 17:53:34 +0700

    Commit - adding CheckInseam

--- validate.js
+++ validate.js
@@ -9,7 +9,7 @@ function checkHeight() {
 
 function checkInseam() {
   let i = (document.getElementById("myInseam").value).trim();
-  if (!isNaN(i)) {
+  if (!isNaN(i)&& h >= 1 && h <= 100) {
     return true;
   } else {
 	return false;
