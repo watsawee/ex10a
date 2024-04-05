f98084079fca6746610a6b0145c6b69022724adc
golf80kg <saranwut435y@gmail.com>
2024-04-06 01:04:23 +0700

    done index and js

--- validate.js
+++ validate.js
@@ -1,35 +1,35 @@
 function checkHeight() {
-  let h = (document.getElementById("myHeight").value).trim();
-  if (!isNaN(h)) {
-    return true;
-  } else {
-	return false;
-  }
-}
-
-function checkInseam() {
-  let i = (document.getElementById("myInseam").value).trim();
-  if (!isNaN(i)) {
-    return true;
-  } else {
-	return false;
-  }
-}
-
-function validateForm(){
-	if(!checkHeight()){
-	  alert("Invalid value for height!");
-	  document.getElementById("myHeight").focus();
+	let h = (document.getElementById("myHeight").value).trim();
+	if (!isNaN(h) && 1<= h && h >= 200) {
+	  return true;
+	} else {
 	  return false;
-	}else{
-		if(!checkInseam()){
-		  alert("Invalid value for inseam!!");
-		  document.getElementById("myInseam").focus();
-		  return false;
-		}else{
-			size = sizeCalculate();
-			alert("Bike size based on you height / insteam  is "+size);
-			return false;
-		}
 	}
-}
+  }
+  
+  function checkInseam() {
+	let i = (document.getElementById("myInseam").value).trim();
+	if (!isNaN(i) && 1 <= i && i >= 100) {
+	  return true;
+	} else {
+	  return false;
+	}
+  }
+  
+  function validateForm(){
+	  if(!checkHeight()){
+		alert("Invalid value for height!");
+		document.getElementById("myHeight").focus();
+		return false;
+	  }else{
+		  if(!checkInseam()){
+			alert("Invalid value for inseam!!");
+			document.getElementById("myInseam").focus();
+			return false;
+		  }else{
+			  size = sizeCalculate();
+			  alert("Bike size based on you height / insteam  is "+size);
+			  return false;
+		  }
+	  }
+  }
